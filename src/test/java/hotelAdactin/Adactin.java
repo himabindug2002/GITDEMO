cf964432240041c4752d766be0a93cf9f3d55e03
himabindug2002 <himabindug2002@gmail.com>
2024-03-22 12:04:01 +0530

    commit

---
+++ src/test/java/hotelAdactin/Adactin.java
@@ -0,0 +1,20 @@
+package hotelAdactin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Adactin {
+
+	public static void main(String[] args) throws Exception {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://adactinhotelapp.com/");
+  	    driver.findElement(By.id("username")).sendKeys("himabindu1417");
+  	    driver.findElement(By.id("password")).sendKeys("2002BINDU");
+  	    driver.findElement(By.id("login")).click();
+
+	}
+
+}
