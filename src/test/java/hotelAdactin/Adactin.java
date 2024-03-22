5d1b31a47b0984f1617fadb1e887f6bc49c74264
himabindug2002 <164126702+himabindug2002@users.noreply.github.com>
2024-03-22 12:44:24 +0530

    Update Adactin.java

--- src/test/java/hotelAdactin/Adactin.java
+++ src/test/java/hotelAdactin/Adactin.java
@@ -15,6 +15,7 @@ public class Adactin {
   	    driver.findElement(By.id("password")).sendKeys("2002BINDU");
   	    driver.findElement(By.id("login")).click();
 		system.out.println("Git");
+		system.out.println("added new line to tigger");
 
 	}
 
